15083b18225c07257bc432af50e6dfcafd1a0f54
Shahzod <shahzodwill@gmail.com>
2025-03-04 17:51:39 +0100

    initial project set up with vite

---
+++ my-ecommerce-store/src/main.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/useCartStore.js";
+import Layout from "./components/Layout.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import ProductPage from "./pages/ProductPage.jsx";
+import CartPage from "./pages/CartPage.jsx";
+import CheckoutSuccess from "./pages/CheckoutSuccess.jsx";
+import ContactPage from "./pages/ContactPage.jsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+// import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/product/:id" element={<ProductPage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/checkout-success" element={<CheckoutSuccess />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </Provider>
+  </StrictMode>
+);
