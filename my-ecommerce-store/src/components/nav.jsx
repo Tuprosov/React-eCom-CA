6049b92bc710a8c62c539b8e362129622f64ecb0
Shahzod <shahzodwill@gmail.com>
2025-03-05 17:02:41 +0100

    create nav

---
+++ my-ecommerce-store/src/components/nav.jsx
@@ -0,0 +1,20 @@
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Menu</Link>
+        </li>
+        <li>
+          <Link to="/products">Menu</Link>
+        </li>
+        <li>
+          <Link to="/products">Menu</Link>
+        </li>
+        <li>
+          <Link to="/products">Menu</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
