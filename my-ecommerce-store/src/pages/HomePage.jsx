85948695b039b20748902af08a9414820e8e7b49
Shahzod <shahzodwill@gmail.com>
2025-03-07 13:15:47 +0100

    update homepage

--- my-ecommerce-store/src/pages/HomePage.jsx
+++ my-ecommerce-store/src/pages/HomePage.jsx
@@ -1,9 +1,13 @@
 import HeroSection from "../components/Hero";
+import Arrivals from "../components/Arrivals";
+import TopSellers from "../components/Top_selling";
 
 function HomePage() {
   return (
     <div>
       <HeroSection />
+      <Arrivals />
+      <TopSellers />
     </div>
   );
 }
