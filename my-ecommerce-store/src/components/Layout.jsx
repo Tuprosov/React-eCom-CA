632414eb431d424d6a7d9c82e71e2fb9088a597b
Shahzod <shahzodwill@gmail.com>
2025-03-08 17:46:14 +0100

    put fetching data to layout component

--- my-ecommerce-store/src/components/Layout.jsx
+++ my-ecommerce-store/src/components/Layout.jsx
@@ -1,12 +1,35 @@
 import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import { API } from "../js/api/api";
+import { API_BASE } from "../js/constants";
 
 function Layout() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const api = new API(`${API_BASE}`);
+
+      try {
+        const data = await api.getProducts();
+        setProducts(data.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array means this will only run once on component mount
+
   return (
     <div>
-      <Header />
-      <Outlet />
+      <Header products={{ products, setProducts }} />
+      <Outlet context={{ products, loading }} />
       <Footer />
     </div>
   );
