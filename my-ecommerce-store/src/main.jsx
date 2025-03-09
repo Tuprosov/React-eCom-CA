cd824dfabb5b8a5c49aac234b84538befc9a2460
Shahzod <shahzodwill@gmail.com>
2025-03-09 10:48:51 +0100

    wrap app with provider

--- my-ecommerce-store/src/main.jsx
+++ my-ecommerce-store/src/main.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./js/store/useCartStore.js";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "./js/store/useCartStore.js";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 );
