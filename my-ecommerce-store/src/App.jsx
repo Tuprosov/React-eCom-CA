8fcb03f2b94b4bf12306bb8a0e1a3b396cf2ceb4
Shahzod <shahzodwill@gmail.com>
2025-03-09 16:42:07 +0100

    build App component

--- my-ecommerce-store/src/App.jsx
+++ my-ecommerce-store/src/App.jsx
@@ -1,29 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store/useCartStore.js";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout.jsx";
 import HomePage from "./pages/HomePage.jsx";
+import ContactPage from "./pages/ContactPage.jsx";
+import "./App.css";
 import ProductPage from "./pages/ProductPage.jsx";
 import CartPage from "./pages/CartPage.jsx";
 import CheckoutSuccess from "./pages/CheckoutSuccess.jsx";
-import ContactPage from "./pages/ContactPage.jsx";
-import "./App.css";
 
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/product/:id" element={<ProductPage />} />
-            <Route path="/cart" element={<CartPage />} />
-            <Route path="/checkout-success" element={<CheckoutSuccess />} />
-            <Route path="/contact" element={<ContactPage />} />
-          </Routes>
-        </Layout>
-      </Router>
-    </Provider>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="product/:id" element={<ProductPage />} />
+        <Route path="shop" element={<div>Shop</div>} />
+        <Route path="onsale" element={<div>Onsale</div>} />
+        <Route path="newarrivals" element={<div>New Arrivals</div>} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="cart" element={<CartPage />} />
+        <Route path="profile" element={<div>Profile</div>} />
+        <Route path="checkout" element={<CheckoutSuccess />} />
+      </Route>
+    </Routes>
   );
 }
 
