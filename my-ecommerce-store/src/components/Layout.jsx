f1e1ad326b26f55b6e81a54d4b9de2d3a339a556
Shahzod <shahzodwill@gmail.com>
2025-03-09 10:49:48 +0100

    removed fetching from layout

--- my-ecommerce-store/src/components/Layout.jsx
+++ my-ecommerce-store/src/components/Layout.jsx
@@ -2,36 +2,20 @@ import { Outlet } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import { API } from "../js/api/api";
-import { API_BASE } from "../js/constants";
+import { ProductProvider } from "../js/productContext";
 
 function Layout() {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const api = new API(`${API_BASE}`);
-
-      try {
-        const data = await api.getProducts();
-        setProducts(data.data);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching products:", error);
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []); // Empty dependency array means this will only run once on component mount
-
   return (
-    <div>
-      <Header products={{ products, setProducts }} />
-      <Outlet context={{ products, loading }} />
-      <Footer />
-    </div>
+    <ProductProvider value={{ products, setProducts }}>
+      <div>
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
+    </ProductProvider>
   );
 }
 
